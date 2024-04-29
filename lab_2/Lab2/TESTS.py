e6dfe570dee66cee4fc54eb985857b3ef8729a79
Tattaboe <polinaohayo314@mail.ru>
2024-04-29 04:31:07 +0400

    End working with frequency test

--- lab_2/Lab2/TESTS.py
+++ lab_2/Lab2/TESTS.py
@@ -1,26 +1,31 @@
 import os
 import math
+from work_file import read_json, write_json
 
 
-from work_file import read_json
-
-
-def frequency_bit_test(path: str, key: str) -> float:
+def frequency_bit_test(path: str, key_f: str) -> float:
     try:
         sub = read_json(path)
-        n = len(sub)
-        sum_xi = sum(-1 if bit == "0" else 1 for bit in sub.get(key))
-        s_n = sum_xi / math.sqrt(n)
+        sum_xi = [-1 if bit == "0" else 1 for bit in sub.get(key_f)]
+        n = len(sum_xi)
+        s_n = sum(sum_xi) / math.sqrt(n)
         p_v = math.erfc(math.fabs(s_n) / math.sqrt(2))
         return p_v
     except Exception as e:
-        print("Error when performing a frequency bitwise test:", e)
-
-
+        print(f"Error frequency_bit_test: {e}")
 
 
 if __name__ == "__main__":
     seq = read_json("paths.json")
     path_s = seq["sequences"]
-    d = frequency_bit_test(path_s, "java")
-    print(d)
+    path_r = seq["result"]
+
+    results = {}
+    keys = ["cpp", "java"]
+
+    for key in keys:
+        result = frequency_bit_test(path_s, key)
+        if result is not None:
+            results[key] = result
+
+    write_json(results, path_r)
--- lab_2/Lab2/work_file.py
+++ lab_2/Lab2/work_file.py
@@ -21,3 +21,20 @@ def read_json(name: str) -> dict:
         print(f"Error reading file {str(e)}")
 
 
+def write_json(data: dict, path: str) -> None:
+    """
+    Write JSON data to a file.
+
+    Args:
+        data (dict): The JSON data to write.
+        path (str): The path to the file to write to.
+    """
+    try:
+        with open(path, "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=1)
+            print(f"The data has been successfully written to the file '{path}'.")
+    except Exception as e:
+        print(f"Error writing to the file: '{e}'.")
+
+
+
