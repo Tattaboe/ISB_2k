3616f137ffcd638ee840ae0fcebe80ae63b63e5a
Tattaboe <polinaohayo314@mail.ru>
2024-05-27 12:38:50 +0400

    implementation of the asymmetry class and asymmetric key serialization

---
+++ lab_3/lab_3/asymmetric.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+
+class Asymmetric:
+
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
+
+    def generate_keys(self) -> None:
+
+        keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        self.private_key = keys
+        self.public_key = keys.public_key()
+
+    def encrypt_text(self, sym_key: bytes) -> bytes:
+
+        encrypted_sym_key = self.public_key.encrypt(sym_key,
+                                                    padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                 algorithm=hashes.SHA256(), label=None))
+        return encrypted_sym_key
+
+    def decrypt_text(self, sym_key: bytes) -> bytes:
+
+        decrypted_sym_key = self.private_key.decrypt(sym_key,
+                                                     padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                  algorithm=hashes.SHA256(), label=None))
+        return decrypted_sym_key
+
--- lab_3/lab_3/seri-deseri.py
+++ lab_3/lab_3/seri-deseri.py
@@ -1,7 +1,10 @@
-import os
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 def ser_sym_key(name: str, key_len: bytes) -> None:
+
     try:
         with open(name, 'wb') as key_file:
             key_file.write(key_len)
@@ -13,6 +16,7 @@ def ser_sym_key(name: str, key_len: bytes) -> None:
 
 
 def deser_sym_key(name: str) -> bytes:
+
     try:
         with open(name, "rb") as file:
             key_len = file.read()
@@ -22,4 +26,67 @@ def deser_sym_key(name: str) -> bytes:
     except Exception as e:
         print(f"Error writing file {str(e)}")
 
+
+def ser_public_key(public_pem: str, public_key: rsa.RSAPublicKey) -> None:
+
+    try:
+        with open(public_pem, "wb") as public_out:
+            public_out.write(
+                public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                )
+            )
+        print(f"The public key has been successfully written to the file '{public_pem}'.")
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+
+def ser_private_key(private_pem: str, private_key: rsa.RSAPrivateKey) -> None:
+
+    try:
+        with open(private_pem, "wb") as private_out:
+            private_out.write(
+                private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                    encryption_algorithm=serialization.NoEncryption(),
+                )
+            )
+            print(f"The private key has been successfully written to the file '{private_pem}'.")
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+
+def deser_public_key(public_pem: str) -> rsa.RSAPublicKey:
+
+    try:
+        with open(public_pem, "rb") as pem_in:
+            public_bytes = pem_in.read()
+        deser_key = load_pem_public_key(public_bytes)
+        return deser_key
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+
+def deser_private_key(private_pem: str) -> rsa.RSAPrivateKey:
+
+    try:
+        with open(private_pem, "rb") as pem_in:
+            private_bytes = pem_in.read()
+        deser_key = load_pem_private_key(
+            private_bytes,
+            password=None,
+        )
+        return deser_key
+    except FileNotFoundError:
+        print("File not found")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
