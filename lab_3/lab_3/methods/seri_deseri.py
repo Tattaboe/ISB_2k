31b6cf3490fab331ebef57ffe51cb9bc019546c1
Tattaboe <polinaohayo314@mail.ru>
2024-05-27 14:23:50 +0400

    main and paths

--- lab_3/lab_3/methods/seri_deseri.py
+++ lab_3/lab_3/methods/seri_deseri.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+
 
 
 def ser_sym_key(name: str, key_len: bytes) -> None:
